d45b5aac88e1c57fc5b6583734faa4d0ea1cab65
varunghai89 <138968781+varunghai89@users.noreply.github.com>
2024-04-01 15:28:40 +0530

    Properties.py

--- ct-api/Properties.py
+++ ct-api/Properties.py
@@ -8,11 +8,11 @@
 class Properties:
     ENV_NAME = 'SLATest Environment'
     CTM_TENANT = 'n/a'
-    HOSTNAME = 'http://172.232.102.173'
+    HOSTNAME = 'http://<CT host IP or URL>'
     TENANT = 'CloudTest'
     COMPS_PATH = '/SLA Test/Compositions/5 Iteration | Availability Test | 2024 | 5 Linodes'
     GRID_NAME = '5 Linode Locations'
-    API_TOKEN = 'a363c666-e155-4d8a-a910-67dd6e1e1efd'
-    USERNAME = 'vghai@akamai.com'
-    PASSWORD = 'Akamai@123'
+    API_TOKEN = 'xxxxxx-exxx-4xxx-axxx-xxxxxxxxxxxx'
+    USERNAME = '<CT username>'
+    PASSWORD = '<CT password>'
     LOGPATH = "./API.log"
